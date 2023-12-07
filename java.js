bb46522d6aabdbbf55d76880be50fec093514dec
nayigannet <145535391+nayigannet@users.noreply.github.com>
2023-12-07 13:12:02 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+document.getElementById("inform").innerHTML = "COME AND EXPLORE MY STRENGTH";
+
+alert("SHEILA PINK welcomes you")
+let fistName, secondName;
+firstName = SHEILLA;
+secondName = PINK;
+
+
+
